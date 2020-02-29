6ccc1a2b21fc3062ed6e5fb8bdace41a279479c9
Travis Weston <tweston@fielty.org>
2020-02-29 15:36:34 -0500

    Make tailwind work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello, Tiffany
                 </div>
 
                 <div class="links">
